62c181d4d6fee75a852f1bea2566d184fe5a4b92
EbenZhang <zhangyingneng@gmail.com>
2015-02-28 08:56:05 +0800

    init impl MainWindowVm

---
+++ PReviewer.Test/TestMainWindowVm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtendedCL;
+using NSubstitute;
+using NSubstitute.Core;
+using NUnit.Framework;
+using Octokit;
+using PReviewer.Domain;
+
+namespace PReviewer.Test
+{
+    [TestFixture]
+    public class TestMainWindowVm
+    {
+        private IGitHubClient _gitHubClient;
+        private IRepositoriesClient _repoClient;
+        private IRepositoryCommitsClient _commitsClient;
+        private IPullRequestsClient _prClient;
+        private MockCompareResult _compareResults;
+        private MainWindowVm _mainWindowVm;
+        private MockPullRequest _pullRequest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _compareResults = new MockCompareResult();
+            _gitHubClient = Substitute.For<IGitHubClient>();
+            _repoClient = Substitute.For<IRepositoriesClient>();
+            _commitsClient = Substitute.For<IRepositoryCommitsClient>();
+            _prClient = Substitute.For<IPullRequestsClient>();
+            _gitHubClient.Repository.Returns(_repoClient);
+            _repoClient.Commits.Returns(_commitsClient);
+            _repoClient.PullRequest.Returns(_prClient);
+
+            _commitsClient.Compare(Arg.Any<string>(),
+                Arg.Any<string>(),
+                Arg.Any<string>(),
+                Arg.Any<string>()
+                ).Returns(Task.FromResult((CompareResult)_compareResults));
+
+            _mainWindowVm = new MainWindowVm(_gitHubClient)
+            {
+                Repository = "repo",
+                Owner = "owner",
+                PullRequestNumber = 11,
+            };
+
+            _pullRequest = new MockPullRequest();
+            _prClient.Get(_mainWindowVm.Owner, _mainWindowVm.Repository, _mainWindowVm.PullRequestNumber).Returns(Task.FromResult((PullRequest)_pullRequest));
+        }
+
+        [Test]
+        public void ShouldBeAbleToGetDiffsForPullRequest()
+        {
+            _mainWindowVm.RetrieveDiffs();
+
+            _prClient.Received(1).Get(_mainWindowVm.Owner, _mainWindowVm.Repository, _mainWindowVm.PullRequestNumber);
+            _commitsClient.Received(1).Compare(_mainWindowVm.Owner, _mainWindowVm.Repository, _pullRequest.Base.Sha, _pullRequest.Head.Sha);
+            Assert.That(_mainWindowVm.Diffs, Contains.Item(_compareResults.File1));
+            Assert.That(_mainWindowVm.Diffs, Contains.Item(_compareResults.File2));
+        }
+
+        [Test]
+        public void ShouldUpdateBusyStatusProperly()
+        {
+            var updateCount = 0;
+            _mainWindowVm.PropertyChanged += (sender, args) =>
+            {
+                if (args.PropertyName == PropertyName.Get((MainWindowVm x) => x.IsProcessing))
+                {
+                    updateCount++;
+                }
+            };
+            _mainWindowVm.RetrieveDiffs();
+
+            Assert.That(updateCount, Is.EqualTo(2));
+            Assert.False(_mainWindowVm.IsProcessing);
+        }
+
+        [Test]
+        public void GivenAnException_BusyStatusShouldBeReset()
+        {
+            _prClient.When(x => x.Get(Arg.Any<string>(),
+                Arg.Any<string>(),
+                Arg.Any<int>()))
+                .Do(x =>
+                {
+                    throw new Exception();
+                });
+            
+#pragma warning disable 1998
+            Assert.Throws<Exception>(async () => _mainWindowVm.RetrieveDiffs());
+#pragma warning restore 1998
+
+            Assert.False(_mainWindowVm.IsProcessing);
+        }
+    }
+
+    class MockPullRequest : PullRequest
+    {
+        public MockPullRequest()
+        {
+            Base = new GitReference("", "", "", "1212", null, null);
+            Head = new GitReference("", "", "", "asdfasdf", null, null);
+        }
+    }
+
+    class MockCompareResult : CompareResult
+    {
+        public GitHubCommitFile File1 = new GitHubCommitFile();
+        public GitHubCommitFile File2 = new GitHubCommitFile();
+        public MockCompareResult()
+        {
+            Files = new List<GitHubCommitFile>()
+            {
+                File1,
+                File2
+            };
+        }
+    }
+}
---
+++ PReviewer/Domain/MainWindowVm.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExtendedCL;
+using GalaSoft.MvvmLight;
+using Octokit;
+
+namespace PReviewer.Domain
+{
+    public class MainWindowVm : ViewModelBase
+    {
+        private readonly IGitHubClient _client;
+        private string _Repository;
+        private string _Owner;
+        private int _PullRequestNumber;
+        private bool _IsProcessing;
+
+        public MainWindowVm(IGitHubClient client)
+        {
+            Diffs = new ObservableCollection<GitHubCommitFile>();
+            _client = client;
+        }
+
+        public string Repository
+        {
+            get { return _Repository; }
+            set
+            {
+                _Repository = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public string Owner
+        {
+            get { return _Owner; }
+            set
+            {
+                _Owner = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public int PullRequestNumber
+        {
+            get { return _PullRequestNumber; }
+            set
+            {
+                _PullRequestNumber = value; 
+                RaisePropertyChanged();
+            }
+        }
+
+        public ObservableCollection<GitHubCommitFile> Diffs { get; set; }
+
+        public bool IsProcessing
+        {
+            get { return _IsProcessing; }
+            set
+            {
+                _IsProcessing = value; 
+                RaisePropertyChanged();
+            }
+        }
+
+        public async void RetrieveDiffs()
+        {
+            IsProcessing = true;
+            try
+            {
+                var repo = _client.Repository;
+                var pr = await repo.PullRequest.Get(Owner, Repository, 11);
+                var commitsClient = repo.Commits;
+                var compareResult = await commitsClient.Compare(Owner, Repository, pr.Base.Sha, pr.Head.Sha);
+                Diffs.Assign(compareResult.Files);
+            }
+            finally
+            {
+                IsProcessing = false;
+            }
+        }
+    }
+}
