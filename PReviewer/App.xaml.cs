456f33e941026167b4b06d68cc65bf72eecdd538
EbenZhang <zhangyingneng@gmail.com>
2015-08-22 19:45:11 +0800

    add error report.

--- PReviewer/App.xaml.cs
+++ PReviewer/App.xaml.cs
@@ -1,11 +1,19 @@
-﻿using System.Linq;
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Navigation;
 using ExtendedCL;
 using GalaSoft.MvvmLight.Threading;
 using Microsoft.Win32;
+using NBug.Core.Submission.Custom;
+using NBug.Events;
+using Octokit;
 using PReviewer.Domain;
 using PReviewer.Model;
+using Application = System.Windows.Application;
 
 namespace PReviewer
 {
@@ -14,10 +22,52 @@ namespace PReviewer
     /// </summary>
     public partial class App : Application
     {
+        private static readonly string LogDir = Path.Combine(PathHelper.ProcessAppDir, "Logs");
+
         public App()
-        {
+        { 
+            NBug.Settings.CustomSubmissionEvent += OnCustomSubmissionEvent;
+            NBug.Settings.Destinations.Add(new Custom());
+            AppDomain.CurrentDomain.UnhandledException += NBug.Handler.UnhandledException;
+            Current.DispatcherUnhandledException += NBug.Handler.DispatcherUnhandledException;
+            NBug.Settings.StoragePath = LogDir;
             DispatcherHelper.Initialize();
             ViewModelLocator.BootStrap();
+        }
+
+        private static async void OnCustomSubmissionEvent(object sender, CustomSubmissionEventArgs customSubmissionEventArgs)
+        {
+            customSubmissionEventArgs.Result = false;
+            var factory = ViewModelLocator.Resolve<IGitHubClientFactory>();
+            var client = factory?.GetClient();
+            if (client == null)
+            {
+                return;
+            }
+            try
+            {
+                var subject = "Product environment exception";
+                if (!string.IsNullOrWhiteSpace(customSubmissionEventArgs.Exception.Message))
+                {
+                    subject = customSubmissionEventArgs.Exception.Message;
+                }
+                await
+                    client.Issue.Create("EbenZhang", "PReviewer",
+                        new NewIssue(subject)
+                        {
+                            Body = customSubmissionEventArgs.Report.ToString() + customSubmissionEventArgs.Exception.ToString(),
+                            Assignee = "EbenZhang",
+                            Labels = { "Production Env Error" },
+                        });
+                customSubmissionEventArgs.File.Dispose();
+
+                File.Delete(Path.Combine(LogDir, customSubmissionEventArgs.FileName));
+                customSubmissionEventArgs.Result = true;
+            }
+            catch
+            {
+                // ignored
+            }
         }
 
         private static string FindAppPath(RegistryKey root, string keyPath, string itemName)
